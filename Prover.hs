7d273dfdf23715a53b8b79dab0afed7c394f26cb
nskowron <nel.skowronek@gmail.com>
2026-06-26 02:20:13 +0200

    added True type and comments

--- Prover.hs
+++ Prover.hs
@@ -6,19 +6,38 @@ module Prover where
 import Data.Typeable
 
 
+-- Assumption
+-- used to store assumed propositions heterogenously in context
+-- problem: retrieving the types later and pattern matching
+-- solutions: casting or try Proxy
 data Assumption where
     Assumption :: (Proposition a) => a -> Assumption
 
+-- Proposition
+-- only types that are an instance of this class can be proven
 class (Typeable a) => Proposition a where
     prove :: [Assumption] -> a
 
+-- True
+instance Proposition () where
+    prove _ = ()
+
+-- Implication
 newtype Impl a b = Impl (a -> b)
 
 instance (Proposition a, Proposition b) => Proposition (Impl a b) where
     prove context = Impl $ \x -> (prove :: [Assumption] -> b) (Assumption x : context)
 
+-- Int
+-- testing the pattern matching solutions
+-- may be used later for FOL
 instance Proposition Int where
     prove (Assumption c : cs) = case cast c of
         Just x  -> x :: Int
         Nothing -> prove cs
     prove [] = error "Not found"
+
+
+-- helper
+eval :: Impl a b -> a -> b
+eval (Impl f) x = f x
