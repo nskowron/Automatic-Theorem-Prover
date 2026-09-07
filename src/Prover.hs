35c4995b4f76c2e70764bf10a10cf463b97a3822
nskowron <nel.skowronek@gmail.com>
2026-09-07 21:48:22 +0200

    added emit to provable again

--- src/Prover.hs
+++ src/Prover.hs
@@ -12,9 +12,12 @@ import Data.Kind ( Type )
 -- === Provable === --
 class Provable a where
     prove :: a
+    emit :: IO ()
 
 instance 
     ( tree ~ FromMaybe Unprovable (MakeNode Search '[] proposition '[])
     , Inferable tree '[] proposition
+    , ShowType '(tree, proposition)
     ) => Provable proposition where
     prove = infer @tree @'[] @proposition HNil
+    emit = putStrLn $ showType @'(tree, proposition)
