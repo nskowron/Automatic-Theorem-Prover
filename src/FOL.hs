d0623d8fce340363b125b423a9809296cf4ee318
nskowron <nel.skowronek@gmail.com>
2026-09-15 23:22:23 +0200

    generalized predicates

--- src/FOL.hs
+++ src/FOL.hs
@@ -9,8 +9,15 @@ import Data.Type.Nat (Nat(..))
 
 
 -- === Quantifiers === --
-type Forall (x :: a) (p :: Type) = Proxy x -> p
+type Forall (x :: a) (p :: a -> Type) = Proxy x -> p x
 type Exists x p = (x, p)
+
+
+-- === Predicate === --
+class Predicate (p :: a -> Type) where
+    type Unwrapped p (x :: a)
+    wrap :: Unwrapped p x -> p x
+    mapWrap :: (Unwrapped p x -> Unwrapped p y) -> (p x -> p y)
 
 
 -- === Experiments === --
@@ -36,11 +43,21 @@ instance
     ) => Inducible (S n) where
     induction base step = step (induction base step)
 
-p :: Forall (x :: Nat) (Z + x :~: x)
-p _ = Refl
+-- p :: Forall (x :: Nat) (Z + x :~: x)
+-- p _ = Refl
 
-p2 :: Forall (x :: Nat) ((Z :*: x) :~: Z)
-p2 _ = Refl
+newtype PlusNZero x = PlusNZero (x + Z :~: x)
+
+instance Predicate PlusNZero where
+    type instance Unwrapped PlusNZero x = (x + Z :~: x)
+    wrap = PlusNZero
+    mapWrap f = \(PlusNZero x) -> PlusNZero (f x)
+
+p1 :: Inducible x => Forall (x :: Nat) PlusNZero
+p1 _ = induction (wrap Refl) (mapWrap (\Refl -> Refl))
+
+-- p2 :: Forall (x :: Nat) ((Z :*: x) :~: Z)
+-- p2 _ = Refl
 
 -- p3 :: Forall (x :: Nat) ((x :*: Z) :~: Z)
 -- p3 _ = induction Refl (\Refl -> Refl)
